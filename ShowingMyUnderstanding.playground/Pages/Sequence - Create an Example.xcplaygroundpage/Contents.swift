87d1ea89a873c00078252a429cc708c7a3c28e1f
Anthony Overing <aovering@lcs.on.ca>
2017-11-24 20:23:10 -0500

    Face with glasses complete

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -26,9 +26,26 @@ canvas.drawShapesWithBorders = false
 // black background
 canvas.fillColor = Color.black
 canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 300)
-
-
-
+//face shape
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 200, height: 200)
+//mouth
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 150, centreY: 100, width: 80, height: 50)
+canvas.fillColor = Color.white
+canvas.drawRectangle(centreX: 150, centreY: 115, width: 150, height: 30)
+//eyes
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 100, centreY: 150, width: 20, height: 20)
+canvas.drawEllipse(centreX: 200, centreY: 150, width: 20, height: 20)
+//glasses
+canvas.drawShapesWithFill = false
+canvas.drawShapesWithBorders = true
+canvas.defaultBorderWidth = 5
+canvas.drawRoundedRectangle(centreX: 100, centreY: 150, width: 80, height: 50)
+canvas.drawRoundedRectangle(centreX: 200, centreY: 150, width: 80, height: 50)
+canvas.defaultLineWidth = 5
+canvas.drawLine(fromX: 140, fromY: 160, toX: 160, toY: 160)
 
 
 
